8591fa16c0bf341aa6bd6010a1ba7bb0ff4bdf77
Jacob437 <--global>
2013-11-05 02:27:11 +0100

    hey

--- Projekt 2/src/Project2Regression.py
+++ Projekt 2/src/Project2Regression.py
@@ -14,6 +14,24 @@ from methods import *
 from scipy.stats import zscore
 import scipy
 
+class ResultHolder:
+    resultNo = 0    
+    results = []
+    
+    def init(__self__):
+        results = []
+        resultNo = 0
+        
+    def addResult(__self__, res):
+        results.add(res)
+        resultNo += 1
+        
+    def printMeanResult(__self__):
+        sum = 0.0
+        for e in results:
+            sum += e
+        return double(sum) / double(resultNo)
+
 #Converts Present and Absent into numbers.
 def convert(s):
     if s == "Present":
@@ -61,7 +79,7 @@ XStandardized = zscore(X, ddof=1)
 
 XPC = getPrincipalComponents(XStandardized)
 
-XPC = getTwoPrincipalComponents(XStandardized)
+#XPC = getTwoPrincipalComponents(XStandardized)
 
 (XWithoutLDL,yWithoutLDL,attributeNamesWithoutAttr) = removeAttribute(X,y,2,attributeNames)
 #forwardSelection(XWithoutLDL,yWithoutLDL,N,M,5,attributeNames,classNames)
@@ -69,7 +87,7 @@ XPC = getTwoPrincipalComponents(XStandardized)
 XWithoutLDLStandardized = zscore(XWithoutLDL, ddof=1)
 
 (X_train,y_train),(X_test,y_test) = getTestAndTrainingSet(X,y)
-(X_train_std,y_train_std),(X_test_std,y_test_std) = getTestAndTrainingSet(XStandardized,y)
+(X_train_PC,y_train_PC),(X_test_PC,y_test_PC) = getTestAndTrainingSet(XStandardized,y)
 
 #linearRegression(X,y,attributeNames,'chd')
 #linearRegression(X,y,attributeNamesWithoutAttr,'ldl')
@@ -97,7 +115,7 @@ Xad = scipy.delete(Xad,1,1) # Tobacco
 Xad = scipy.delete(Xad,0,1) # SBP
 
 X2PC = np.copy(XPC)
-X2PC = X2PC[:,0:1]
+X2PC = X2PC[:,0:2]
 
 #X2PC = scipy.delete(X2PC,8,1) # PC9
 #X2PC = scipy.delete(X2PC,7,1) # PC8
@@ -109,6 +127,20 @@ X2PC = X2PC[:,0:1]
 
 
 (X_train_ad,y_train_ad),(X_test_ad,y_test_ad) = getTestAndTrainingSet(Xad,y)
+(X_train_2PC,y_train_2PC),(X_test_2PC,y_test_2PC) = getTestAndTrainingSet(X2PC,y)
+
+(_,_,attributeNamesXad) = removeAttribute(X,y,6,attributeNames)
+(_,_,attributeNamesXad) = removeAttribute(X,y,3,attributeNamesXad)
+(_,_,attributeNamesXad) = removeAttribute(X,y,1,attributeNamesXad)
+(_,_,attributeNamesXad) = removeAttribute(X,y,0,attributeNamesXad)
+
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,8,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,7,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,6,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,5,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,4,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,3,attributeNames)
+(_,_,attributeNamesX2PC) = removeAttribute(X,y,2,attributeNames)
 
 
 #artificialNeuralNetwork(Xad, y, N, noAttributes-4)
@@ -120,32 +152,32 @@ s2 = "Attributes of X selected according to result of forward selection."
 s3 = "X represented by principal components"
 s4 = "X represented by two most important principal components."
 #
-K = 4
 
+#K=4
 #artificialNeuralNetwork(X,y,N,noAttributes, K=K, s=s1)
 #artificialNeuralNetwork(Xad,y,N,noAttributes-4, K=K, s=s2)
 #artificialNeuralNetwork(XPC,y,N,noAttributes, K=K, s=s3)
 #artificialNeuralNetwork(X2PC,y,N,2, K=K, s=s4)
 
 
-predictLinearRegression(X,y)
+#predictLinearRegression(X,y)
 
-#logisticRegression(X,y,s=s1)
-#logisticRegression(Xad,y,s=s2)
-#logisticRegression(XPC,y,s=s3)
-#logisticRegression(X2PC,y,s=s4)
-
-decisionTree(X,y,attributeNames,classNames,"Decision_Tree_X.gvz",s=s1)
-decisionTree(Xad,y,attributeNames,classNames,"Decision_Tree_Xad.gvz",s=s2)
-decisionTree(XPC,y,attributeNames,classNames,"Decision_Tree_XPC.gvz",s=s3)
-decisionTree(X2PC,y,attributeNames,classNames,"Decision_Tree_X2PC.gvz",s=s4)
+#logisticRegression(X,y,X_train, y_train, X_test, y_test, s=s1)
+#logisticRegression(Xad,y,X_train_ad, y_train_ad, X_test_ad, y_test_ad, s=s2)
+#logisticRegression(XPC,y,X_train_PC, y_train_PC, X_test_PC, y_test_PC, s=s3)
+#logisticRegression(X2PC,y,X_train_2PC, y_train_2PC, X_test_2PC, y_test_2PC, s=s4)
 #
-#kNearestNeighbours(X,y,N,C,s=s1)
-#kNearestNeighbours(Xad,y,N,C,s=s2)
-#kNearestNeighbours(XPC,y,N,C,s=s3)
-#kNearestNeighbours(X2PC,y,N,C,s=s4)
+#decisionTree(X,y,attributeNames,classNames,"Decision_Tree_X.gvz",s=s1)#X,X_train,y_train,X_test,y_test)
+#decisionTree(Xad,y,attributeNamesXad,classNames,"Decision_Tree_Xad.gvz",s=s1)#,X_train_ad,y_train_ad,X_test_ad,y_test_ad)
+#decisionTree(XPC,y,attributeNames,classNames,"Decision_Tree_XPC.gvz",s=s1)#,X_train_PC,y_train_PC,X_test_PC,y_test_PC)
+#decisionTree(X2PC,y,attributeNamesX2PC,classNames,"Decision_Tree_X2PC.gvz",s=s1)#,X_train_2PC,y_train_2PC,X_test_2PC,y_test_2PC)
 #
-#plotKNearestNeighbours(classNames, X, y, C, s=s1, k=15)
-#plotKNearestNeighbours(classNames, Xad, y, C, s=s2, k=14)
-#plotKNearestNeighbours(classNames, XPC, y, C, s=s3, k=24)
-#plotKNearestNeighbours(classNames, X2PC, y, C, DoPrincipalComponentAnalysis = True,s=s4,neighbours=30)
+(XK,e1) = kNearestNeighbours(X,y,C,s=s1)
+(XKad,e2) = kNearestNeighbours(Xad,y,C,s=s2)
+(XKPC,e3) = kNearestNeighbours(XPC,y,C,s=s3)
+(XK2PC,e4) = kNearestNeighbours(X2PC,y,C,s=s4)
+#
+plotKNearestNeighbours(classNames, X, y, C, s=s1, K=15)
+plotKNearestNeighbours(classNames, Xad, y, C, s=s2, K=14)
+plotKNearestNeighbours(classNames, XPC, y, C, s=s3, K=24)
+plotKNearestNeighbours(classNames, X2PC, y, C, DoPrincipalComponentAnalysis = True,s=s4,K=30)
